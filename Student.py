c389d39304c0afcb19c7484f026581365c519df5
peter <s3896790@student.rmit.edu.au>
2021-10-14 15:53:49 +1100

    get & set for student

--- Student.py
+++ Student.py
@@ -12,4 +12,17 @@ class Student:
         self.curEnoll = curEnoll
 
 
-        
+    def getStudent(ID):
+        student = Student.studentID == ID
+        return student
+    def setStudent( self, name, studentID, dob, programCode, accedemicHist = [], studyPlan = [], curEnoll = [] ):
+        self.name = name 
+        self.studentID = studentID
+        self.dob = dob
+        self.programeCode = programCode
+        # accedemic history is a lsit of tupples
+        self.accedemicHist = accedemicHist
+        # studdy plan history is a lsit of tupples
+        self.studyPlan = studyPlan
+        self.curEnoll = curEnoll
+    
