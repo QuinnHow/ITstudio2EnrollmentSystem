cf7849b32e2ee51556ef9e72a1bf9761deaad85d
James Hijazin <jameshijazin@gmail.com>
2021-10-16 18:40:49 +1100

    Added one row to semester.csv

--- Semester.py
+++ Semester.py
@@ -2,5 +2,5 @@ class Semester:
     def __init__(self,ID,cOfferigns = []):
        ## in the format of S2 year(year is the year)
         self.ID = ID
-       ## tupple (Name , max student avalibility , current students) 
+       ## tuple (name, max student avalibility, current students) 
         self.cOfferings = cOfferigns
