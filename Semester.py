e4b6ccab473f1e7489684551b6f9692bc33fde67
James Hijazin <jameshijazin@gmail.com>
2021-10-16 18:41:12 +1100

    Spelling error

--- Semester.py
+++ Semester.py
@@ -1,6 +1,6 @@
 class Semester:
-    def __init__(self,ID,cOfferigns = []):
+    def __init__(self,ID,cOfferings = []):
        ## in the format of S2 year(year is the year)
         self.ID = ID
        ## tuple (name, max student avalibility, current students) 
-        self.cOfferings = cOfferigns
+        self.cOfferings = cOfferings
