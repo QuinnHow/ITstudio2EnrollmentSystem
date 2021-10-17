a489376301b3606e3ac04d5273dbf21cb4ce36b7
peter <s3896790@student.rmit.edu.au>
2021-10-17 14:18:49 +1100

    testing something in main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # this is just here so we ahve a spot to wrok off of
+# testing something 
+finish = True
+while finish:
+    inp = input('What would you like to do?\n')
+    # a way to exit the program
+    if inp.lower() == 'quit': break
+    #put the different calls here e.g navigating through the menu... 
+## save all files or save files when finished editing them?
+print ('exiting program') 
