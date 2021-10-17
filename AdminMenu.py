2ebaf89c1baca5234b5c5fa72fc3af7997cb2d60
Quinn How <s3899122@student.rmit.edu.au>
2021-10-17 15:09:58 +1100

    set up part 2

---
+++ AdminMenu.py
@@ -0,0 +1,30 @@
+import Student
+import Program
+import Course
+import Semester
+
+## Add/Remove/Amendastudent(Youwillalsoneedtoconsidertheeffectonotherclassinstancese.g., program, semester offerings.)
+
+
+## Add/Remove/Amendacourse(Youwillalsoneedtoconsidertheeffectonotherclassinstancese.g.,semester offerings.)
+
+
+## Add/Remove/Amendaprogram(Youwillalsoneedtoconsidertheeffectonotherclassinstancese.g., students.)
+
+
+## Add/Remove/Amendasemester(Youwillalsoneedtoconsidertheeffectonotherclassinstancese.g., students)
+
+
+## Query student information including academic history and current enrolment
+
+
+## Allow manual amendment of the study plan for a student
+
+
+## Validate a students study plan
+
+
+## generate a student plan for a student of minimum length
+
+
+## for a particular course offering display a sorted list of students
---
+++ StudentMenu.py
@@ -0,0 +1,16 @@
+import Student
+import Program
+import Course
+import Semester
+
+
+## Display academic history and current enrolment
+
+
+## Querying course or program information
+
+
+## Enrol/UnEnrol in a current offering (You will need to consider the pre-req and the impact of an enrollment.)
+
+
+## Exit
