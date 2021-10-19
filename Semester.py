3fcec5c96c707c15111f29ba0d0d7b86fb9e8709
James Hijazin <jameshijazin@gmail.com>
2021-10-19 16:28:37 +1100

    Added a string function

--- Semester.py
+++ Semester.py
@@ -5,6 +5,9 @@ class Semester:
        ## tuple (name, max student avalibility, current students) 
         self.cOfferings = cOfferings
 
+    def __str__(self):
+        return '{}, {}'.format(self.ID, self.cOfferings)
+
     def getSemester(ID): ## may be changing this later
         semester = Semester.ID = ID
         return semester
