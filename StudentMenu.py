e43b51458687c163f36255c84092109b852e52c7
peter <s3896790@student.rmit.edu.au>
2021-10-20 15:56:29 +1100

    student menu changes

--- StudentMenu.py
+++ StudentMenu.py
@@ -1,23 +1,24 @@
-import Student
-import Program
-import Course
-import Semester
+def studentMenu (courses,programs,semesters,students):
+    finish = True
+    while finish:
+        inp = input('welcome to the student menu, What would you like to do?\n')
+        ## returning to the base menu 
+        if inp.lower() == 'exit': break
+
+    print ('returning to main meu') 
+    import Student
+    import Program
+    import Course
+    import Semester
 
 
-## Display academic history and current enrolment
+    ## Display academic history and current enrolment
 
 
-## Querying course or program information
+    ## Querying course or program information
 
 
-## Enrol/UnEnrol in a current offering (You will need to consider the pre-req and the impact of an enrollment.)
+    ## Enrol/UnEnrol in a current offering (You will need to consider the pre-req and the impact of an enrollment.)
 
 
-## Exit
-finish = True
-while finish:
-    inp = input('welcome to the student menu, What would you like to do?\n')
-    ## returning to the base menu 
-    if inp.lower() == 'exit': break
-
-print ('returning to main meu') 
+    ## Exit
