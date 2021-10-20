6f060a06d0e92aad89ae0bdc101fdafdd86ba813
peter <s3896790@student.rmit.edu.au>
2021-10-20 12:55:40 +1100

    adding add/remove functions

--- Program.py
+++ Program.py
@@ -6,3 +6,23 @@ class Program:
         self.core = core
         self.elective = elective
 
+def add_core(self,course):
+    self.core = self.core.append(course)
+def add_eletive(self,course):
+    self.elective = self.elective.append(course)
+    
+def remove_core(self,course):
+    ## checking if the course exists within the program before removing 
+    if self.core[course]:
+        self.core = self.core.pop(course)
+def remove_elective(self,course):
+    if self.elective[course]:
+        self.elective = self.elective.pop(course)
+
+def __str__(self):
+    print('Program code:',(self.code))
+    print('Credit points:',(self.creditPoints))
+    ## we want the corse code to be printed???????
+    ## dose this use the strmethod from course????
+    print('Core courses:',(self.core))
+    print('Elective courses:',(self.elective))
