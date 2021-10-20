3e0863cfab77398897d6cdb0a0c9d6133a1848f3
peter <s3896790@student.rmit.edu.au>
2021-10-20 15:32:35 +1100

    fixing str mehton in program

--- Program.py
+++ Program.py
@@ -6,23 +6,29 @@ class Program:
         self.core = core
         self.elective = elective
 
-def add_core(self,course):
-    self.core = self.core.append(course)
-def add_eletive(self,course):
-    self.elective = self.elective.append(course)
+    def add_core(self,course):
+        self.core = self.core.append(course)
+    def add_eletive(self,course):
+        self.elective = self.elective.append(course)
     
-def remove_core(self,course):
+    def remove_core(self,course):
     ## checking if the course exists within the program before removing 
-    if self.core[course]:
-        self.core = self.core.pop(course)
-def remove_elective(self,course):
-    if self.elective[course]:
-        self.elective = self.elective.pop(course)
+        if self.core[course]:
+            self.core = self.core.pop(course)
+    def remove_elective(self,course):
+        if self.elective[course]:
+            self.elective = self.elective.pop(course)
 
-def __str__(self):
-    print('Program code:',(self.code))
-    print('Credit points:',(self.creditPoints))
-    ## we want the corse code to be printed???????
-    ## dose this use the strmethod from course????
-    print('Core courses:',(self.core))
-    print('Elective courses:',(self.elective))
+    def __str__(self):
+        return 'Program code: ' + str(self.code) + '\n' + 'Credit points: ' + str(self.creditPoints) + '\n' + 'Core courses: ' + str(self.core) + '\n' + 'Elective courses: ' + str(self.elective)
+# testing str function#############################################
+#cheese = [3,2,1]
+#cheese.append(4)
+#temp = Program(123,45,cheese,('cheese','burger'))
+#print (temp)
+# output was :
+#Program code: 123
+#Credit points: 45
+#Core courses: [3, 2, 1, 4]
+#Elective courses: ('cheese', 'burger')
+###################################################################
