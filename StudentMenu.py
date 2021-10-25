c5d9990906caae17e7200f0ff1ce5e04dfe7ae5c
peter <s3896790@student.rmit.edu.au>
2021-10-25 17:14:37 +1100

    login function mostly finished we need to move the running of the menu to the bottom

--- StudentMenu.py
+++ StudentMenu.py
@@ -1,6 +1,9 @@
 def studentMenu (courses,programs,semesters,students):
-   
-    finish = True
+    ## needs to be after the function deffinitions
+    success = login(students)
+    if success:
+        finish = True
+    else :finish = False
     while finish:
         inp = input('welcome to the student menu, What would you like to do?\n')
         ## returning to the base menu 
@@ -13,7 +16,16 @@ def studentMenu (courses,programs,semesters,students):
     #import Semester
 
     ## login Peter
-    
+    def login(students):
+        print('Please sign in')
+        studentnum = input('Enter your student number\n')
+        pword = input('Enter your password\n')
+        for i in students:
+            if i.studentID == studentnum:
+                ### we need a pasword for each student!!!!!!!!!!!
+                if pword == i.pword:
+                    return True
+        return False
 
     ## Display academic history and current enrolment Quinn
 
