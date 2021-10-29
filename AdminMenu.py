2364f29544493284f84d501224a64119ff578b67
peter <s3896790@student.rmit.edu.au>
2021-10-29 12:42:03 +1100

    admin password fix

--- AdminMenu.py
+++ AdminMenu.py
@@ -1,6 +1,6 @@
 import csv
 
-def adminmenu(courses,programs,semesters,students):
+def adminmenu(courses,programs,semesters,students,passwords):
 ##import Student
 ##import Program
 ##import Course
@@ -131,7 +131,7 @@ def adminmenu(courses,programs,semesters,students):
     def helpadmin():
         print('If you would like to\n\nAdd/Remove/Amendastudent - enter arast (the student)\nAdd/Remove/Amendacourse - enter arac (the course)\nAdd/Remove/Amendaprogram - enter arap (the program)\nAdd/Remove/Amendasemester - enter arase (the semester)\nQuery student information including academic history and current enrolment - enter query (the student)\nAllow manual amendment of the study plan for a student - enter ama(the student)\nValidate a students study plan - validate (the student)\nGenerate a student plan for a student of minimum length - enter generate study (the student)\nFor a particular course offering display a sorted list of students - enter display students (specific course offering)\nReturn to the main menu - enter exit')
 
-    success = login(students)
+    success = login(passwords)
     if success:
         finish = True
         print('\nwelcome to the admin menu\n')
